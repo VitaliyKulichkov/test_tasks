7574388b907b95dd1b350a628732779fa15a720d
VitaliyKulichkov <shizgiz1337@gmail.com>
2022-11-15 11:55:26 +0300

    Added func main in task3 Added task4.py

--- task3.py
+++ task3.py
@@ -1,12 +1,23 @@
 import json
-lst =[]
-with open("values.json", "r") as f:
-    data = json.loads(f.read())
-    for i in data['values']:
-        lst.append(i['value'])
+def main():
+    lst =[]
+    with open("values.json", "r") as f:
+        data = json.loads(f.read())
+        for i in data['values']:
+            lst.append(i['value'])
 
-#for el in lst: TODO!
-   # lst.append({'value': el})
+    with open("tests.json", "r") as f1:
+        data2= json.loads(f1.read())
+        data2['value'] = lst[0]
+        j = 1
+        for i in data2['values']:
+            i['value'] = lst[j]
+            j+=1
 
-with open('tests.json', 'w') as f:
-    json.dump(lst, f)
+
+    report = json.dumps(data2)
+    with open("report.json", "w") as f2:
+        f2.write(report)
+
+if __name__ == '__main__':
+    main()
--- task4.py
+++ task4.py
@@ -1,12 +1,16 @@
-#Имя файла
-filename = input('Введите название файла: ')
-#Чтение данных из файла
-text = open(filename,'r').readlines()
-a = []
-for line in text:
-    a.append(int(line.strip()))
-print(a)
 
-#Нахождение минимального количества ходов
-m = sorted(a)[len(a) // 2]
-print(sum(abs(v - m) for v in a))
+def main():
+    #Имя файла
+    filename = input('Введите название файла: ')
+    #Чтение данных из файла
+    text = open(filename,'r').readlines()
+    a = []
+    for line in text:
+        a.append(int(line.strip()))
+
+    #Нахождение минимального количества ходов
+    m = sorted(a)[len(a) // 2]
+    print(sum(abs(v - m) for v in a))
+
+if __name__ == '__main__':
+    main()
